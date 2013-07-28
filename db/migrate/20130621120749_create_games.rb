5e7f4b32e5a99de7598c212b8ab0cd9af4136417
Tej Singh <wctej89@gmail.com>
2013-07-27 19:14:03 -0700

    removed default for current_board in migration

--- db/migrate/20130621120749_create_games.rb
+++ db/migrate/20130621120749_create_games.rb
@@ -5,27 +5,7 @@ class CreateGames < ActiveRecord::Migration
       t.integer :challenger_id
       t.integer :winner_id
       t.boolean :user_turn, default: true
-      t.text :current_board, default: <<-DOC 
-<div class='board'>
-  <div class='row'>
-    <div id='square0' class='square'></div>
-    <div id='square1' class='square'></div>
-    <div id='square2' class='square'></div>
-  </div>
-
-  <div class='row'>
-    <div id='square3' class='square'></div>
-    <div id='square4' class='square'></div>
-    <div id='square5' class='square'></div>
-  </div>
-
-  <div class='row'>
-    <div id='square6' class='square'></div>
-    <div id='square7' class='square'></div>
-    <div id='square8' class='square'></div>
-  </div>
-</div>
-DOC
+      t.text :current_board
     end
   end
 end
